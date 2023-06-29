82b22dfa990c33e0d2849992a0dcd34b836e6984
Ruslan Kryskiv <kryskiv23@gmail.com>
2023-06-29 14:19:43 +0300

    Start of a new project auto shop!
    Development is progressing according to pattern MVC
    - Development view model

---
+++ src/view/GreatingClient.java
@@ -0,0 +1,42 @@
+package view;
+
+import client.Client;
+
+import java.util.Scanner;
+
+public class GreatingClient implements GreetingsView{
+    Client client;
+    Scanner scanner = new Scanner(System.in);
+
+    public GreatingClient(Client client) {
+        this.client = client;
+    }
+
+    @Override
+    public int greeating() {
+        System.out.println(client.getName() + " Welcome to car shop!");
+        System.out.println("""
+                ___What do you want to do in our store?___
+                
+                1. View cars
+                2. Buy a car
+                
+                Please make your choice!
+                """);
+        return scanner.nextInt();
+
+    }
+
+    public void filterOption(int options){
+        switch (options){
+            case 1 -> {
+                // write logic here
+            }
+            case 2 -> {
+                // write logic here
+            }
+        }
+    }
+
+
+}
